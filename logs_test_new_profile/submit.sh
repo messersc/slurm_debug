b54227f450b85a4e3f38a2218848bc71108ac5d9
Clemens Messerschmidt <clemens.messerschmidt@bihealth.de>
2022-12-14 08:37:16 +0100

    reorg

--- logs_test_new_profile/submit.sh
+++ logs_test_new_profile/submit.sh
@@ -6,9 +6,10 @@
 #SBATCH --time=48:10:00
 #SBATCH --mem-per-cpu=300M
 #SBATCH --output=%x-%j.log
+#SBATCH --reservation=cmesser_m_16
 
 export SBATCH_DEFAULTS="--output=slurm_logs/%x-%j.log"
 date
 #srun snakemake --use-conda -j200 --profile=/data/gpfs-1/work/users/cmesser_m/dev/snakemake_profile_patched
-srun snakemake --use-conda -j200 --profile=cubi-v1
+srun snakemake --use-conda -j200 --profile=cubi-v1 --cluster "sbatch --reservation=cmesser_m_16"
 date
